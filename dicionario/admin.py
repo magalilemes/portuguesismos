c9f6f210a05d0d249a0a1183af7e8476eb07a821
Magali Lemes <magalilemes00@gmail.com>
2021-12-11 15:04:10 -0300

    Adicionar campo categoria para termos

--- dicionario/admin.py
+++ dicionario/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ClasseGramatical, Termo
+from .models import Categoria, ClasseGramatical, Termo
 
+admin.site.register(Categoria)
 admin.site.register(ClasseGramatical)
 admin.site.register(Termo)
--- dicionario/models.py
+++ dicionario/models.py
@@ -8,13 +8,19 @@ class ClasseGramatical(models.Model):
     def __str__(self):
         return self.nome
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
 class Termo(models.Model):
     """Modelo representando um termo estrangeiro."""
     termo = models.CharField(max_length=200)
     traducao = models.CharField(max_length=200)
     definicao = models.TextField(help_text='Insira uma definição em português do termo.')
     classe_gramatical = models.ForeignKey(ClasseGramatical, on_delete=models.SET_NULL, null=True)
-    categoria = models.CharField(max_length=100)
+    categoria = models.ManyToManyField(Categoria)
     equivalencia = models.IntegerField()
 
     slug = models.SlugField(null=False, unique=True)
